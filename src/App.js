a18f50cd5259144e4d63be8a8f705e4f657dd314
boyan <106580794+bobyTopG@users.noreply.github.com>
2025-07-25 23:08:51 +0300

    Refactor quiz navigation to use useReducer for state management and improve progress tracking

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { useState } from "react";
-import DateCounter from "./DateCounter";
+import { useReducer, useState } from "react";
+// import DateCounter from "./DateCounter";
 import Header from "./Header";
 import data from "./questions.json";
 
-export default function App() {
-  const [index, setIndex] = useState(0);
-  const question = data.questions[index];
-  const [answer, setAnswer] = useState(null);
+function reducer(state, action) {
+  switch (action.type) {
+    case "next":
+      return { ...state, index: state.index + 1 };
+    default:
+      throw Error("Error");
+  }
+}
 
+export default function App() {
+  const initaData = { index: 0 };
+  const [state, dispatch] = useReducer(reducer, initaData);
+  const { index } = state;
+
+  const question = data.questions[index];
+
+  const [answer, setAnswer] = useState(null);
   return (
     <div className="App">
       <Header></Header>
+      <ProgressBar index={index} numQuestions={data.questions.length} />
       <Question question={question} />
       <Options
         options={question.options}
@@ -18,8 +31,21 @@ export default function App() {
         correctAnswer={question.correctOption}
         setAnswer={setAnswer}
       />
-      <NextBtn setIndex={setIndex} />
+      <NextBtn setAnswer={setAnswer} onChange={dispatch} />
     </div>
+  );
+}
+
+function ProgressBar({ index, numQuestions }) {
+  const percentage = Math.round(((index + 1) / numQuestions) * 100);
+
+  return (
+    <header className="progress">
+      <progress max="100" value={percentage}></progress>
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestions}
+      </p>
+    </header>
   );
 }
 
@@ -27,21 +53,45 @@ function Question({ question }) {
   return <p className="question">{question.question}</p>;
 }
 
-function Options({ options }) {
+function Options({ options, answer, correctAnswer, setAnswer }) {
   return (
-    <div className="options">
-      {options.map((opt, i) => (
-        <button className="btn btn-option" key={i}>
-          {opt}
-        </button>
-      ))}
+    <div>
+      {options.map((opt, i) => {
+        let isSelected = answer === i;
+        let isCorrect = i === correctAnswer;
+        let className = "btn btn-option";
+        if (answer !== null) {
+          if (isCorrect) className += " correct";
+        }
+        if (isSelected && isCorrect) className += " correct";
+        else if (isSelected && !isCorrect) className += " wrong";
+
+        return (
+          <div className="options">
+            <button
+              disabled={answer !== null}
+              className={className}
+              key={i}
+              onClick={() => setAnswer(i)}
+            >
+              {opt}
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-function NextBtn({ setIndex }) {
+function NextBtn({ setAnswer, onChange }) {
   return (
-    <button className="btn btn-ui" onClick={() => setIndex((i) => i + 1)}>
+    <button
+      className="btn btn-ui"
+      onClick={() => {
+        setAnswer(null);
+        onChange({ type: "next" });
+      }}
+    >
       Next
     </button>
   );
