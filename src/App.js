30c011aebecf96046c6b2b306d019232b285143b
boyan <106580794+bobyTopG@users.noreply.github.com>
2025-07-26 10:50:22 +0300

    Implement state management for quiz answers and points using useReducer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 // import DateCounter from "./DateCounter";
 import Header from "./Header";
 import data from "./questions.json";
+import { type } from "@testing-library/user-event/dist/type";
 
 function reducer(state, action) {
   switch (action.type) {
     case "next":
       return { ...state, index: state.index + 1 };
+    case "setAnswer":
+      return { ...state, answer: action.payload };
+    case "addPoints":
+      return { ...state, points: state.points + 10 };
+    case "tick":
+      return { ...state, time: state.time - 1 };
     default:
       throw Error("Error");
   }
 }
 
 export default function App() {
-  const initaData = { index: 0 };
+  const initaData = { index: 0, points: 0, time: 70, answer: null };
   const [state, dispatch] = useReducer(reducer, initaData);
-  const { index } = state;
+  const { index, points, time, answer } = state;
 
   const question = data.questions[index];
 
-  const [answer, setAnswer] = useState(null);
+  useEffect(() => {
+    if (state.time === 0) return;
+
+    const timer = setInterval(() => {
+      dispatch({ type: "tick" });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [state.time]);
+
   return (
     <div className="App">
       <Header></Header>
       <ProgressBar index={index} numQuestions={data.questions.length} />
+      <Points points={points} />
       <Question question={question} />
       <Options
         options={question.options}
         answer={answer}
         correctAnswer={question.correctOption}
-        setAnswer={setAnswer}
+        dispatch={dispatch}
       />
-      <NextBtn setAnswer={setAnswer} onChange={dispatch} />
+      <Timer time={time} />
+      <NextBtn answer={answer} dispatch={dispatch} />
     </div>
   );
+}
+
+function Points({ points }) {
+  return <p className="progress">Points: {points}</p>;
+}
+function Timer({ time }) {
+  return <p className="timer">⏳ {time}s</p>;
 }
 
 function ProgressBar({ index, numQuestions }) {
@@ -53,7 +78,7 @@ function Question({ question }) {
   return <p className="question">{question.question}</p>;
 }
 
-function Options({ options, answer, correctAnswer, setAnswer }) {
+function Options({ options, answer, correctAnswer, dispatch }) {
   return (
     <div>
       {options.map((opt, i) => {
@@ -63,8 +88,9 @@ function Options({ options, answer, correctAnswer, setAnswer }) {
         if (answer !== null) {
           if (isCorrect) className += " correct";
         }
-        if (isSelected && isCorrect) className += " correct";
-        else if (isSelected && !isCorrect) className += " wrong";
+        if (isSelected && isCorrect) {
+          className += " correct";
+        } else if (isSelected && !isCorrect) className += " wrong";
 
         return (
           <div className="options">
@@ -72,7 +98,12 @@ function Options({ options, answer, correctAnswer, setAnswer }) {
               disabled={answer !== null}
               className={className}
               key={i}
-              onClick={() => setAnswer(i)}
+              onClick={() => {
+                dispatch({ type: "setAnswer", payload: i });
+                if (correctAnswer === i) {
+                  dispatch({ type: "addPoints", payload: 10 });
+                }
+              }}
             >
               {opt}
             </button>
@@ -83,13 +114,15 @@ function Options({ options, answer, correctAnswer, setAnswer }) {
   );
 }
 
-function NextBtn({ setAnswer, onChange }) {
-  return (
+function NextBtn({ answer, dispatch }) {
+  return answer === null ? (
+    <></>
+  ) : (
     <button
       className="btn btn-ui"
       onClick={() => {
-        setAnswer(null);
-        onChange({ type: "next" });
+        dispatch({ type: "setAnswer", payload: null });
+        dispatch({ type: "next" });
       }}
     >
       Next
