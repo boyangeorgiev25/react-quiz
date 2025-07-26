5f6e2c7e9db2cc66a82679c29b4cd85e6e5b739f
boyan <106580794+bobyTopG@users.noreply.github.com>
2025-07-26 11:31:07 +0300

    Refactor component structure by moving Header and Error components to the components directory and reintroducing DateCounter with state management

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 // import DateCounter from "./DateCounter";
-import Header from "./Header";
+import Header from "./componenets/Header";
 import data from "./questions.json";
-import { type } from "@testing-library/user-event/dist/type";
 
 function reducer(state, action) {
   switch (action.type) {
